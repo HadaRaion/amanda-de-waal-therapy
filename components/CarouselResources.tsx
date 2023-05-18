3dd3816967624f262c41e01bc47e64b5d73b3cd3
hada <sweet.raion@gmail.com>
2023-05-17 18:29:27 -0700

    Complete resources carousel section.

--- components/CarouselResources.tsx
+++ components/CarouselResources.tsx
@@ -1,12 +1,31 @@
 import { getFeaturedResources } from '@/services/resources';
+import Image from 'next/image';
+import MultiCarousel from './MultiCarousel';
 
 export default async function CarouselResources() {
 	const resources = await getFeaturedResources();
 	return (
-		<section>
-			{resources.map(resource => (
-				<p key={resource.path}>{resource.title}</p>
-			))}
+		<section className="flex flex-col md:flex-row border-y border-black md:items-end gap-16 md:gap-20 py-32 md:py-0">
+			<h2 className="md:py-32 lg:py-36 link md:w-1/4 text-center md:text-right">
+				My Resources
+			</h2>
+			<MultiCarousel>
+				{resources.map(resource => (
+					<div
+						key={resource.path}
+						className="aspect-[2/3] relative before:absolute before:z-10 before:inset-0 before:bg-light/30 border border-black">
+						<Image
+							src={`/images/resources/${resource.path}.jpg`}
+							alt={`Cover image of ${resource.title}`}
+							fill
+							// width="0"
+							// height="0"
+							// sizes="100vw"
+							// className="w-full h-auto"
+						/>
+					</div>
+				))}
+			</MultiCarousel>
 		</section>
 	);
 }
---
+++ components/MultiCarousel.tsx
@@ -0,0 +1,40 @@
+'use client';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+	superLargeDesktop: {
+		breakpoint: { max: 4000, min: 3000 },
+		items: 8,
+	},
+	desktop: {
+		breakpoint: { max: 3000, min: 1024 },
+		items: 7,
+	},
+	tablet: {
+		breakpoint: { max: 1024, min: 464 },
+		items: 4,
+	},
+	mobile: {
+		breakpoint: { max: 464, min: 0 },
+		items: 2,
+	},
+};
+
+type Props = {
+	children: React.ReactNode;
+};
+
+export default function MultiCarousel({ children }: Props) {
+	return (
+		<Carousel
+			infinite
+			responsive={responsive}
+			arrows={false}
+			autoPlay
+			itemClass="mx-4 md:mx-8"
+			containerClass="md:py-32 lg:py-36 md:w-3/4 border-l border-black">
+			{children}
+		</Carousel>
+	);
+}
