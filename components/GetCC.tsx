59d71dcd1e56572c0d49777baae537a8f7ee189f
hada <sweet.raion@gmail.com>
2023-05-27 14:19:44 -0700

    Change design for CC form

--- components/GetCC.tsx
+++ components/GetCC.tsx
@@ -5,16 +5,18 @@ export default function GetComplimentaryConsultation() {
 	return (
 		<section className="py-32 lg:py-36 border-t border-black">
 			<Container className="flex flex-col items-center">
-				<h2 className="font-serif text-3xl leading-snug lg:text-4xl xl:text-5xl xl:leading-tight mt-12">
+				<h2 className="font-serif text-3xl leading-snug lg:text-4xl xl:text-5xl xl:leading-tight mt-12 text-center">
 					Complimentary Consultation
 				</h2>
-				<p className="mt-8 mb-20 text-center lg:w-3/5 ">
-					Finding the support that feels best to you can sometimes feel like a
-					strenuous process.
-					<br />
-					It’s important that you can get a bit of a feel for who I am, and how
-					I show up, before deciding to work with me.
-					<br />
+				<p className="mt-8 mb-20 text-center xl:w-3/5 ">
+					<span className="br">
+						Finding the support that feels best to you can sometimes feel like a
+						strenuous process.{' '}
+					</span>
+					<span className="br">
+						It’s important that you can get a bit of a feel for who I am, and
+						how I show up, before deciding to work with me.{' '}
+					</span>
 					Please reach out, so we can find a time to connect!
 				</p>
 				<GetComplimentaryConsultationForm className="md:w-3/5" />
